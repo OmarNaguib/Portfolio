cbac75e0924dee90b82c07000bd4c138a1f0ecee
Omar <55961349+OmarNaguib@users.noreply.github.com>
2023-08-08 20:20:57 +0300

    Create Basic components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './components/Header'
+import Projects from './components/Projects'
+import Contact from './components/Contact'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div></div>
+    <>
+    <Header></Header>
+    <Projects></Projects>
+    <Contact></Contact>
+    </>
   )
 }
 
