d83ab2f4adba5ef85b7d4685a9ace53ab203e5d5
Omar <55961349+OmarNaguib@users.noreply.github.com>
2023-08-09 12:37:06 +0300

    Add projects content

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,3 +1,27 @@
+import data from "../assets/data";
+
 export default function Projects() {
-  return <div></div>;
+  const projects = data.map((dataItem) => {
+    return (
+      <div key={dataItem.name} className="project">
+        <img src={dataItem.screenshot} alt={dataItem.name + " screenshot"} />
+        <div>
+          <h2>{dataItem.name}</h2>
+          <a href={dataItem.repo} target="_blank" rel="noreferrer">
+            repo
+          </a>
+          <a href={dataItem.live} target="_blank" rel="noreferrer">
+            live
+          </a>
+          <p>{dataItem.description}</p>
+        </div>
+      </div>
+    );
+  });
+  return (
+    <section>
+      <h1>My Work</h1>
+      <div className="projects">{projects}</div>
+    </section>
+  );
 }
