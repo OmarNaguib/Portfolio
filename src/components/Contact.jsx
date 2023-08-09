e863cacd4caaa47dfe06a15c5aa4f70fca73c79b
Omar <55961349+OmarNaguib@users.noreply.github.com>
2023-08-09 18:36:29 +0300

    Add contact content

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,3 +1,38 @@
+import profilePic from "./../assets/profile.jpg";
+
 export default function Contact() {
-  return <div></div>;
+  return (
+    <section>
+      <div className="contact-info">
+        <h2>Contact me</h2>
+        <p>
+          Please get in touch if you think our work could be mutually
+          beneficial!
+        </p>
+        <p>
+          Mansoura, <br /> Dakahlia, Egypt
+        </p>
+        <p>+2 01000549518</p>
+        <p>omar.nagiub2001@gmail.com</p>
+        <div className="links">
+          <a
+            href="https://github.com/OmarNaguib"
+            target="_blank"
+            rel="noreferrer"
+          >
+            github
+          </a>
+          <a
+            href="https://www.linkedin.com/in/omarnaguib0/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            linkedIn
+          </a>
+          <a href="">twitter</a>
+        </div>
+      </div>
+      <img src={profilePic} alt="" />
+    </section>
+  );
 }
