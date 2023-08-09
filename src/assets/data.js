e489e49fc1ba2f2c29b13fdf6927200c802bd8db
Omar <55961349+OmarNaguib@users.noreply.github.com>
2023-08-09 11:52:32 +0300

    Add project names and pics

---
+++ src/assets/data.js
@@ -0,0 +1,64 @@
+import shoppingCartPic from "./screenshots/shoppingCart.png";
+import memoryGamePic from "./screenshots/memoryGame.png";
+import cvBuilderPic from "./screenshots/cvBuilder.png";
+import battleshipPic from "./screenshots/battleship.png";
+import todoPic from "./screenshots/todo.png";
+import weatherAppPic from "./screenshots/weatherApp.png";
+
+const Project = (name, screenshot, repo, description, live) => {
+  return {
+    name,
+    screenshot,
+    repo,
+    description,
+    live,
+  };
+};
+
+const shoppingCart = Project(
+  "Shopping Cart",
+  shoppingCartPic,
+  "https://github.com/OmarNaguib/Shopping-Cart",
+  "A fully tested react app that uses client side routing",
+  "https://omarnaguib.github.io/Shopping-Cart/"
+);
+
+const memoryGame = Project(
+  "Memory Game",
+  memoryGamePic,
+  "https://github.com/OmarNaguib/Shopping-Cart",
+  "A fully tested react app that uses client side routing",
+  "https://omarnaguib.github.io/Shopping-Cart/"
+);
+
+const cvBuilder = Project(
+  "Shopping Cart",
+  cvBuilderPic,
+  "https://github.com/OmarNaguib/Shopping-Cart",
+  "A fully tested react app that uses client side routing",
+  "https://omarnaguib.github.io/Shopping-Cart/"
+);
+
+const battleship = Project(
+  "Shopping Cart",
+  battleshipPic,
+  "https://github.com/OmarNaguib/Shopping-Cart",
+  "A fully tested react app that uses client side routing",
+  "https://omarnaguib.github.io/Shopping-Cart/"
+);
+
+const todo = Project(
+  "Shopping Cart",
+  todoPic,
+  "https://github.com/OmarNaguib/Shopping-Cart",
+  "A fully tested react app that uses client side routing",
+  "https://omarnaguib.github.io/Shopping-Cart/"
+);
+
+const weatherApp = Project(
+  "Shopping Cart",
+  weatherAppPic,
+  "https://github.com/OmarNaguib/Shopping-Cart",
+  "A fully tested react app that uses client side routing",
+  "https://omarnaguib.github.io/Shopping-Cart/"
+);
