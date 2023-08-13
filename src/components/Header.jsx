fe70440311a9389a0bbf19b9c7d9d2dba7cf08e8
Omar <55961349+OmarNaguib@users.noreply.github.com>
2023-08-13 15:45:08 +0300

    Fix faulty responsive transition

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,3 @@
-import headerLandscapePic from "./../assets/personal/header-landscape.jpg";
 import headerPortraitPic from "./../assets/personal/header-portrait.jpg";
 import headerSquarePic from "./../assets/personal/header-square.jpg";
 
@@ -7,11 +6,11 @@ export default function Header() {
     <section className="header">
       <div className="left-section">
         <picture>
-          <source srcSet={headerSquarePic} media="(min-width: 562px)" />
           <source srcSet={headerPortraitPic} media="(max-width: 562px)" />
+          <source srcSet={headerSquarePic} media="(min-width: 562px)" />
           <img src={headerSquarePic} alt="presenting" />
         </picture>
-        {/* <img src={headerSquarePic} alt="" /> */}
+
         <h1>Omar Naguib</h1>
       </div>
       <div className="about">
