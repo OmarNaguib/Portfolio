7a1001060b89e18a45d6eb03a327dd2b00e761fa
omar <55961349+OmarNaguib@users.noreply.github.com>
2023-10-21 11:02:32 +0300

    Improve accessibility

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,36 +4,37 @@ import openIcon from "./../assets/icons/open-in-new.svg";
 export default function Projects() {
   const projects = data.map((dataItem) => {
     return (
-      <li
-        key={dataItem.name}
-        id={dataItem.name}
-        className="project"
-        aria-labelledby={dataItem.id + "Header"}
-        aria-describedby={dataItem.id + "Description"}
-      >
-        <img src={dataItem.screenshot} alt={dataItem.name + " screenshot"} />
-        <div className="row">
-          <h3 id={dataItem.id + "Header"}>{dataItem.name}</h3>
-          <div className="links">
-            <a
-              href={dataItem.repo}
-              target="_blank"
-              rel="noreferrer"
-              aria-label={dataItem.name + " repo"}
-            >
-              <img src={githubIcon} alt="" className="icon" />
-            </a>
-            <a
-              href={dataItem.live}
-              target="_blank"
-              rel="noreferrer"
-              aria-label={dataItem.name + " live website"}
-            >
-              <img src={openIcon} alt="" className="icon" />
-            </a>
+      <li key={dataItem.name}>
+        <article
+          id={dataItem.name}
+          className="project"
+          aria-labelledby={dataItem.id + "Header"}
+          aria-describedby={dataItem.id + "Description"}
+        >
+          <img src={dataItem.screenshot} alt={dataItem.name + " screenshot"} />
+          <div className="row">
+            <h3 id={dataItem.id + "Header"}>{dataItem.name}</h3>
+            <div className="links">
+              <a
+                href={dataItem.repo}
+                target="_blank"
+                rel="noreferrer"
+                aria-label={dataItem.name + " repo"}
+              >
+                <img src={githubIcon} alt="" className="icon" />
+              </a>
+              <a
+                href={dataItem.live}
+                target="_blank"
+                rel="noreferrer"
+                aria-label={dataItem.name + " live website"}
+              >
+                <img src={openIcon} alt="" className="icon" />
+              </a>
+            </div>
           </div>
-        </div>
-        <p id={dataItem.id + "Description"}>{dataItem.description}</p>
+          <p id={dataItem.id + "Description"}>{dataItem.description}</p>
+        </article>
       </li>
     );
   });
